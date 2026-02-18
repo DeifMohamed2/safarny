831c160348a7bb0cd03d6b4c5f01de79b5380de4
Mohamed-AbdElGhaffar-AbdElGhaffar <ma7497591@gmail.com>
2026-02-18 22:10:16 +0200

    company profile

--- src/pages/CompanyProfile/CompanyProfile.tsx
+++ src/pages/CompanyProfile/CompanyProfile.tsx
@@ -2,10 +2,12 @@ import Quotes from './components/Quotes'
 import TripsSection from '../../components/shared/TripsSection'
 import TripCard from '@/components/shared/TripCard'
 import trip from "@assets/trip/trip.jpg"
+import company from "@assets/companies/company.png"
 import { useState } from 'react'
 
 // import { useTranslation } from 'react-i18next'
 import TripsCarousel from '@/components/shared/TripsCarousel'
+import Header from './components/Header'
 
 const CompanyProfile = () => {
   // const { i18n } = useTranslation()
@@ -26,8 +28,26 @@ const CompanyProfile = () => {
     { id: "5", title: "Dahab Adventure", location: "Dahab, Egypt" },
     { id: "6", title: "Alexandria Weekend", location: "Alexandria, Egypt" },
   ]
+  const destinationsCovered = [
+    "Sharm El Sheikh",
+    "Hurghada",
+    "Dahab",
+    "Marsa Alam",
+    "El Gouna",
+    "Soma Bay"
+  ]
   return (
     <div className='relative min-h-dvh sm:h-full max-w-340 w-full px-4 pt-32.5 mb-4 mx-auto'>
+      <Header 
+        title='Wanderlust Journeys'
+        subTitle='Specializing in beach & leisure trips'
+        rate={4.8}
+        reviews={2347}
+        image={company}
+        description='Wanderlust Journeys has been crafting unforgettable travel experiences since 2010. We specialize in luxury beach destinations and leisure travel packages across the Red Sea region. Our expert team curates every detail to ensure your vacation is seamless, relaxing, and truly memorable.'
+        destinationsCovered={destinationsCovered}
+        contactNumber='201227375904'
+      />
       <TripsSection
         title={
           <>
